31bf747a720088016c96c723e2b022f868e86cda
Vasya <br2876@bs.amalnet.k12.il>
2026-07-19 16:08:30 +0300

    i made main activity be an upper class for 3 other classes ill do later

--- app/src/main/java/com/example/daatabace_issue/MainActivity.java
+++ app/src/main/java/com/example/daatabace_issue/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.daatabace_issue;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.AdapterView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        menu.add(1, 1, 50, "Add");
+        menu.add(1 , 2, 100, "Show");
+        menu.add(1, 3, 200, "Filter");
+        menu.add(1, 4, 300, "Credit");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+        Intent intent = WhereToGo(item.getTitle().toString());
+        finish();
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
+    }
+
+    public Intent WhereToGo(String item){
+        return new Intent(this, Adder.class);
+    }
+
+
 }
